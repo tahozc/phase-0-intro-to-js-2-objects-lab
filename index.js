e597052b2d1503a3ab5fb4de43ccc2c51fec04fa
Taha <tahaozcilingir@msn.com>
2023-06-04 20:55:55 -0400

    Performs Operations using objects

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+const employee = {}
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    const newObj = Object.assign({}, employee, { [key]: value })
+    return newObj
+}
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    employee[key] = value;
+    return employee
+}
+const deleteFromEmployeeByKey = (employee, key) => {
+    const newObj = { ...employee };
+    delete newObj[key];
+    return newObj
+}
+const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key]
+    return employee
+}
